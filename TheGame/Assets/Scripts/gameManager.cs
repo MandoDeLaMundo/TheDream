b794aef2da08a7bfc9305f543103f447effeb924
Montiel_Armando <98683036+MandoDeLaMundo@users.noreply.github.com>
2025-05-12 20:26:52 -0400

    ADDED pause menu

---
+++ TheGame/Assets/Scripts/gameManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public GameObject player;
+    public playerController playerScript;
+
+    public bool isPaused;
+
+    float timeScaleOrig;
+    int gameGoalCount;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+        timeScaleOrig = Time.timeScale;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                StatePause();
+                menuActive = menuPause;
+                menuActive.SetActive(isPaused);
+            }
+
+            else if (menuActive == menuPause)
+                StateUnpause();
+        }
+    }
+
+    public void StatePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void StateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+    public void YouLose()
+    {
+        StatePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+
+    public void UpdateGameGoal(int amount)
+    {
+        gameGoalCount += amount;
+
+        if (gameGoalCount <= 0)
+        {
+            StatePause();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+        }
+    }
+}
