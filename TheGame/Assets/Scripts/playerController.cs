75839315c30b84318c5436bbf2117ab109731e43
TristinS5 <tlsneary@student.fullsail.edu>
2025-05-13 13:00:16 -0400

    Made Jump and Sprint on playerController

--- TheGame/Assets/Scripts/playerController.cs
+++ TheGame/Assets/Scripts/playerController.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlTypes;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
@@ -5,6 +6,14 @@ public class playerController : MonoBehaviour
     [SerializeField] CharacterController controller;
 
     [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+
+    //Jump variables
+    [SerializeField] int jumpMax;
+    [SerializeField] int jumpForce;
+    [SerializeField] int Gravity;
+    int jumpCount;
+    Vector3 playerVel;
 
     Vector3 moveDir;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,12 +26,44 @@ public class playerController : MonoBehaviour
     void Update()
     {
         Movement();
+        sprint();
     }
 
     void Movement()
     {
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel = Vector3.zero;
+        }
+
         moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
         
         controller.Move(moveDir * speed * Time.deltaTime);
+
+        jump();
+        controller.Move(playerVel * Time.deltaTime);
+        playerVel.y -= Gravity * Time.deltaTime;
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax) 
+        {
+            jumpCount++;
+            playerVel.y = jumpForce;
+        }
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed += sprintMod;
+        }
+        if (Input.GetButtonUp("Sprint"))
+        {
+            speed -= sprintMod;
+        }
     }
 }
