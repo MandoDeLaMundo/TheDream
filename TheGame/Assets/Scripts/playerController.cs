861f0ed662f845730aa03218de42382edc1f062c
Montiel_Armando <98683036+MandoDeLaMundo@users.noreply.github.com>
2025-05-13 15:06:17 -0400

    ADDED start room and pit damage

--- TheGame/Assets/Scripts/playerController.cs
+++ TheGame/Assets/Scripts/playerController.cs
@@ -5,148 +5,151 @@ using Unity.VisualScripting;
 
 public class playerController : MonoBehaviour
 {
-    [SerializeField] CharacterController controller;
-    [SerializeField] LayerMask ignoreLayer;
+	[SerializeField] CharacterController controller;
+	[SerializeField] LayerMask ignoreLayer;
 
-    [SerializeField] int HP;
-    int HPOrig;
+	[SerializeField] int HP;
+	int HPOrig;
 
-    [SerializeField] int speed;
-    [SerializeField] int sprintMod;
+	[SerializeField] int speed;
+	[SerializeField] int sprintMod;
 
-    [SerializeField] bool shooting;
-    [SerializeField] int shootDamage;
-    [SerializeField] float shootRate;
-    [SerializeField] int shootDist;
-    float shootTimer;
+	[SerializeField] bool shooting;
+	[SerializeField] int shootDamage;
+	[SerializeField] float shootRate;
+	[SerializeField] int shootDist;
+	float shootTimer;
 
-    [SerializeField] bool teleporting;
-    [SerializeField] float teleportRate;
-    [SerializeField] int teleportDist;
+	[SerializeField] bool teleporting;
+	[SerializeField] float teleportRate;
+	[SerializeField] int teleportDist;
 
-    [SerializeField] int jumpMax;
-    [SerializeField] int jumpForce;
-    [SerializeField] int Gravity;
-    int jumpCount;
-    Vector3 playerVel;
+	[SerializeField] int jumpMax;
+	[SerializeField] int jumpForce;
+	[SerializeField] int Gravity;
+	int jumpCount;
+	Vector3 playerVel;
 
 
 
-    Vector3 moveDir;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        HPOrig = HP;
-    }
+	Vector3 moveDir;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		HPOrig = HP;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (shooting)
-        {
-            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
-        }
-        if (teleporting)
-        {
-            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * teleportDist, Color.blue);
-        }
-        Movement();
-        sprint();
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (shooting)
+		{
+			Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
+		}
+		if (teleporting)
+		{
+			Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * teleportDist, Color.blue);
+		}
+		Movement();
+		sprint();
+	}
 
-    void Movement()
-    {
-        shootTimer += Time.deltaTime;
+	void Movement()
+	{
+		shootTimer += Time.deltaTime;
 
-        if (controller.isGrounded)
-        {
-            jumpCount = 0;
-            playerVel = Vector3.zero;
-        }
+		if (controller.isGrounded)
+		{
+			jumpCount = 0;
+			playerVel = Vector3.zero;
+		}
 
-        moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
-        
-        controller.Move(moveDir * speed * Time.deltaTime);
+		moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
 
-        jump();
+		controller.Move(moveDir * speed * Time.deltaTime);
 
-        controller.Move(playerVel * Time.deltaTime);
-        playerVel.y -= Gravity * Time.deltaTime;
-        if (Input.GetButton("Fire1") && shootTimer >= shootRate && shooting)
-        {
-            shoot();
-        }
-        if(Input.GetButton("Fire2") && shootTimer >= teleportRate && teleporting)
-        {
-            teleportbyclick();
-        }
-    }
+		jump();
 
-    void jump()
-    {
-        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax) 
-        {
-            jumpCount++;
-            playerVel.y = jumpForce;
-        }
-    }
+		controller.Move(playerVel * Time.deltaTime);
+		playerVel.y -= Gravity * Time.deltaTime;
+		if (Input.GetButton("Fire1") && shootTimer >= shootRate && shooting)
+		{
+			shoot();
+		}
+		if (Input.GetButton("Fire2") && shootTimer >= teleportRate && teleporting)
+		{
+			teleportbyclick();
+		}
+	}
 
-    void sprint()
-    {
-        if (Input.GetButtonDown("Sprint"))
-        {
-            speed += sprintMod;
-        }
-        if (Input.GetButtonUp("Sprint"))
-        {
-            speed -= sprintMod;
-        }
-    }
+	void jump()
+	{
+		if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+		{
+			jumpCount++;
+			playerVel.y = jumpForce;
+		}
+	}
 
-    void shoot()
-    {
-        shootTimer = 0;
+	void sprint()
+	{
+		if (Input.GetButtonDown("Sprint"))
+		{
+			speed += sprintMod;
+		}
+		if (Input.GetButtonUp("Sprint"))
+		{
+			speed -= sprintMod;
+		}
+	}
 
-        RaycastHit hit;
+	void shoot()
+	{
+		shootTimer = 0;
 
-        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
-        {
-            Debug.Log(hit.collider.name);
-            IDamage dmg = hit.collider.GetComponent<IDamage>();
+		RaycastHit hit;
 
-            if (dmg != null)
-            {
-                dmg.TakeDMG(shootDamage);
-            }
-        }
-    }
+		if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+		{
+			Debug.Log(hit.collider.name);
+			IDamage dmg = hit.collider.GetComponent<IDamage>();
 
-    void teleportbyclick()
-    {
-        shootTimer = 0;
+			if (dmg != null)
+			{
+				dmg.TakeDMG(shootDamage);
+			}
+		}
+	}
 
-        RaycastHit hit;
+	void teleportbyclick()
+	{
+		shootTimer = 0;
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
-        {
-            Debug.Log(hit.collider.name);
-            Vector3 teleportpos = hit.point;
-            if (Vector3.Distance(transform.position, teleportpos) <= teleportDist)
-            {
-                teleportpos.y = transform.position.y;
-                transform.position = teleportpos;
-            }
-        }
-    }
+		RaycastHit hit;
 
-    public void TakeDMG(int amount)
-    {
-        HP -= amount;
+		if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+		{
+			Debug.Log(hit.collider.name);
+			Vector3 teleportpos = hit.point;
+			if (Vector3.Distance(transform.position, teleportpos) <= teleportDist)
+			{
+				teleportpos.y = transform.position.y;
+				transform.position = teleportpos;
+			}
+		}
+	}
 
-        if(HP == 0)
-        {
-            gameManager.instance.YouLose();
-        }
-    }
+	public void TakeDMG(int amount)
+	{
+		HP -= amount;
+
+		if (controller.transform.position.y < 0)
+			HP = 0;
+
+		if (HP == 0)
+		{
+			gameManager.instance.YouLose();
+		}
+	}
 
 }
