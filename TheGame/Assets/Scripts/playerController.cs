63b2d5e57e9abd3a5fb218b3e78388bbadafcd93
TristinS5 <tlsneary@student.fullsail.edu>
2025-05-13 13:38:54 -0400

    Started a shoot for the playerController

--- TheGame/Assets/Scripts/playerController.cs
+++ TheGame/Assets/Scripts/playerController.cs
@@ -1,19 +1,28 @@
 using System.Data.SqlTypes;
 using UnityEngine;
+using System.Collections;
 
 public class playerController : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
+    [SerializeField] LayerMask ignoreLayer;
 
     [SerializeField] int speed;
     [SerializeField] int sprintMod;
 
-    //Jump variables
+    [SerializeField] bool shooting;
+    [SerializeField] int shootDamage;
+    [SerializeField] float shootRate;
+    [SerializeField] int shootDist;
+    float shootTimer;
+
     [SerializeField] int jumpMax;
     [SerializeField] int jumpForce;
     [SerializeField] int Gravity;
     int jumpCount;
     Vector3 playerVel;
+
+
 
     Vector3 moveDir;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,12 +34,18 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (shooting)
+        {
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
+        }
         Movement();
         sprint();
     }
 
     void Movement()
     {
+        shootTimer += Time.deltaTime;
+
         if (controller.isGrounded)
         {
             jumpCount = 0;
@@ -42,8 +57,13 @@ public class playerController : MonoBehaviour
         controller.Move(moveDir * speed * Time.deltaTime);
 
         jump();
+
         controller.Move(playerVel * Time.deltaTime);
         playerVel.y -= Gravity * Time.deltaTime;
+        if (Input.GetButton("Fire1") && shootTimer >= shootRate && shooting)
+        {
+            shoot();
+        }
     }
 
     void jump()
@@ -64,6 +84,18 @@ public class playerController : MonoBehaviour
         if (Input.GetButtonUp("Sprint"))
         {
             speed -= sprintMod;
+        }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+        {
+
         }
     }
 }
