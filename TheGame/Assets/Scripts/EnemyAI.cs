1fa572effb78bf4bf1799f0e9f9d0dbdb213c6bd
Montiel_Armando <98683036+MandoDeLaMundo@users.noreply.github.com>
2025-05-14 17:37:53 -0400

    ADDED Melee to the enemy script

--- TheGame/Assets/Scripts/EnemyAI.cs
+++ TheGame/Assets/Scripts/EnemyAI.cs
@@ -3,106 +3,123 @@ using System.Collections;
 using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour, IDamage
 {
-    [SerializeField] Renderer model;
-    [SerializeField] NavMeshAgent agent;
+	[SerializeField] Renderer model;
+	[SerializeField] NavMeshAgent agent;
 
-    [SerializeField] int HP;
-    [SerializeField] int faceTargetSpeed;
+	[SerializeField] int HP;
+	[SerializeField] int faceTargetSpeed;
 
-    [SerializeField] Transform shootPos;
-    [SerializeField] GameObject projectile;
-    [SerializeField] float shootRate;
-    [SerializeField] int damageAmount;
-    Color colorOrig;
+	[SerializeField] Transform shootPos;
+	[SerializeField] GameObject projectile;
+	[SerializeField] float shootRate;
+	[SerializeField] int rangeDmgAmount;
 
-    Vector3 playerDir;
+	[SerializeField] float meleeRate;
+	[SerializeField] float meleeDistance;
+	[SerializeField] int meleeDmgAmount;
 
-    float shootTimer;
+	Color colorOrig;
 
-    bool playerInRange;
+	Vector3 playerDir;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        colorOrig = model.material.color;
-         gameManager.instance.UpdateGameGoal(1);
-    }
+	float shootTimer;
+	float meleeTimer;
 
-    // Update is called once per frame
-    void Update()
-    {
+	bool playerInRange;
 
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		colorOrig = model.material.color;
+		gameManager.instance.UpdateGameGoal(1);
+	}
 
-        if (playerInRange)
-        {
-              playerDir = (gameManager.instance.player.transform.position - transform.position);
+	// Update is called once per frame
+	void Update()
+	{
+		if (playerInRange)
+		{
+			meleeTimer += Time.deltaTime;
 
-              agent.SetDestination(gameManager.instance.player.transform.position);
+			playerDir = (gameManager.instance.player.transform.position - transform.position);
 
-
-            if (agent.remainingDistance <= agent.stoppingDistance)  
-            {
-                faceTarget();
-            }
-        }
+			agent.SetDestination(gameManager.instance.player.transform.position);
 
 
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collision detected with: " + collision.collider.name);
-        if (collision.collider.CompareTag("Player"))
-        {
-            IDamage dmg = collision.collider.GetComponent<IDamage>();
-            if (dmg != null)
-            {
-                dmg.TakeDMG(damageAmount);
-            }
-        }
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = true;
-        }
-    }
+			if (agent.remainingDistance <= agent.stoppingDistance)
+			{
+				faceTarget();
+			}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = false;
-        }
-    }
+			if (playerDir.magnitude <= meleeDistance && meleeTimer >= meleeRate)
+			{
+				attackPlayer();
+			}
+		}
 
-    public void TakeDMG(int amount)
-    {
-        HP -= amount;
 
-         agent.SetDestination(gameManager.instance.player.transform.position);
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Collision detected with: " + collision.collider.name);
+		if (collision.collider.CompareTag("Player"))
+		{
+			IDamage dmg = collision.collider.GetComponent<IDamage>();
+			if (dmg != null)
+			{
+				dmg.TakeDMG(rangeDmgAmount);
+			}
+		}
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			playerInRange = true;
+		}
+	}
 
-        if (HP <= 0)
-        {
-            gameManager.instance.UpdateGameGoal(-1);
-            Destroy(gameObject);
-        }
-        else
-        {
-            StartCoroutine(flashRed());
-        }
-    }
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			playerInRange = false;
+		}
+	}
 
-    IEnumerator flashRed()
-    {
-        model.material.color = Color.red;
-        yield return new WaitForSeconds(0.05f);
-        model.material.color = colorOrig;
-    }
+	public void TakeDMG(int amount)
+	{
+		HP -= amount;
 
-    void faceTarget()
-    {
-        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
-        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
-    }
+		agent.SetDestination(gameManager.instance.player.transform.position);
+
+		if (HP <= 0)
+		{
+			gameManager.instance.UpdateGameGoal(-1);
+			Destroy(gameObject);
+		}
+		else
+		{
+			StartCoroutine(flashRed());
+		}
+	}
+
+	IEnumerator flashRed()
+	{
+		model.material.color = Color.red;
+		yield return new WaitForSeconds(0.05f);
+		model.material.color = colorOrig;
+	}
+
+	void faceTarget()
+	{
+		Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+		transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+	}
+
+	private void attackPlayer()
+	{
+		gameManager.instance.player.GetComponent<playerController>().TakeDMG(meleeDmgAmount);
+		meleeTimer = 0;
+	}
 }
