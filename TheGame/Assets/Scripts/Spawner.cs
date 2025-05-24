165cb67fe3f5955ec4f9745a5a77912968a8428c
Donell Denson <donelldenson108@icloud.com>
2025-05-24 16:36:23 -0400

    checking in enemyAI added bool to check if enemy sentry added full rotation for sentries while not moving allowed all enemies to shoot up and down added spawner script added sentry plant prefab

---
+++ TheGame/Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] int numToSpawn;
+    [SerializeField] int spawnRate;
+    [SerializeField] Transform[] spawnPos;
+
+    int spawnCount;
+    float spawnTimer;
+    bool startSpawning;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager.instance.UpdateGameGoal(numToSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startSpawning)
+        {
+            spawnTimer = Time.deltaTime;
+            if (spawnTimer >= spawnRate && spawnCount < numToSpawn)
+            {
+                spawn();
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startSpawning = true;
+        }
+    }
+
+    void spawn()
+    {
+        int arrayPos = Random.Range(0, spawnPos.Length);
+        Instantiate(objectToSpawn, spawnPos[arrayPos].position, spawnPos[arrayPos].rotation);
+        spawnCount++;
+        spawnTimer = 0;
+    }
+}
