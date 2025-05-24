127b10c66dfe7831d4517b592747e95660a2e075
Montiel_Armando <98683036+MandoDeLaMundo@users.noreply.github.com>
2025-05-24 15:55:04 -0400

    UPDATED the items

--- TheGame/Assets/Scripts/pickup.cs
+++ TheGame/Assets/Scripts/pickup.cs
@@ -8,7 +8,7 @@ public class pickup : MonoBehaviour
 	[SerializeField] spellStats spell;
 	[SerializeField] itemStats item;
 
-	bool isFirstTimePickedup;
+	//bool isFirstTimePickedup;
 
 	private void OnTriggerEnter(Collider other)
 	{
@@ -17,9 +17,15 @@ public class pickup : MonoBehaviour
 		if (toPickup != null)
 		{
 			if (type == pickupType.weapon)
+			{
+				Debug.Log("Picked up a weapon");
 				toPickup.GetSpellStats(spell);
+			}
 			else if (type == pickupType.item)
+			{
+				Debug.Log("Picked up an item");
 				toPickup.GetItemStats(item);
+			}
 			
 			Destroy(gameObject);
 		}
