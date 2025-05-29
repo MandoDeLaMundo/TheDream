9c2b0c1f727ff1458781ba58e9eaaa529f1e1597
RoyLe <lehoangphuc168@gmail.com>
2025-05-28 22:41:15 -0400

    spellStats script update and teleport balance

--- TheGame/Assets/Scripts/spellStats.cs
+++ TheGame/Assets/Scripts/spellStats.cs
@@ -8,7 +8,7 @@ public class spellStats : ScriptableObject
     [Range(1, 20)] public int shootDMG;
     [Range(0.0f, 25.0f)] public float shootRate;
     [Range(1, 1000)] public int shootDist;
-    [Range(1, 200)] public int manaCost;
+    [Range(0, 200)] public int manaCost;
     [Range(0.0f, 100.0f)] public float spellRadius;
 
     public ParticleSystem hitEffect;
