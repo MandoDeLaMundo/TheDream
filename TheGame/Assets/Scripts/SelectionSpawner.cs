455ac9914c5720741a8c9a217873e4b7ae9ba71f
TristinS5 <tlsneary@student.fullsail.edu>
2025-06-13 03:28:11 -0400

    Fixed the Selection Spawner and Made a spawnStats scriptableobject and made a spell spawner and a item spawner

--- TheGame/Assets/Scripts/SelectionSpawner.cs
+++ TheGame/Assets/Scripts/SelectionSpawner.cs
@@ -1,22 +1,48 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.U2D;
+using UnityEngine.UI;
 
 public class SelectionSpawner : MonoBehaviour
 {
-    [SerializeField] List<GameObject> objectsToSpawn = new List<GameObject>();
+    [SerializeField] List<spawnStats> spawnList = new List<spawnStats>();
+    GameObject spawnObject;
     [SerializeField] int numToSpawn;
     [SerializeField] int spawnRate;
     [SerializeField] Transform[] spawnPos;
     int objectListPos;
+
+    [SerializeField] GameObject Canvas;
+    [SerializeField] Image image;
+
+    [SerializeField] GameObject leftButtonFilled;
+    [SerializeField] GameObject leftButtonHole;
+    [SerializeField] GameObject rightButtonFilled;
+    [SerializeField] GameObject rightButtonHole;
 
     int spawnCount;
     float spawnTimer;
     bool playerInTrigger;
     bool startSpawner;
 
+    [SerializeField] float buttonTime;
+    float buttonTimer;
+
+    void Start()
+    {
+        image.sprite = spawnList[objectListPos].sprite;
+        spawnObject = spawnList[objectListPos].pickup;
+
+        leftButtonFilled.SetActive(true);
+        leftButtonHole.SetActive(false);
+        rightButtonFilled.SetActive(true);
+        rightButtonHole.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
+        buttonTimer += Time.deltaTime;
         if (playerInTrigger)
         {
             if (Input.GetButtonDown("Interact"))
@@ -29,7 +55,21 @@ public class SelectionSpawner : MonoBehaviour
             {
                 spawn();
             }
-            selectEnemies();
+            selectEverything();
+
+            if (Input.GetKeyDown("q"))
+            {
+                playerController.instance.enabled = true;
+            }
+
+            if (buttonTimer >= buttonTime && leftButtonFilled.activeSelf == false || buttonTimer >= buttonTime && rightButtonFilled.activeSelf == false)
+            {
+                leftButtonFilled.SetActive(true);
+                leftButtonHole.SetActive(false);
+                rightButtonFilled.SetActive(true);
+                rightButtonHole.SetActive(false);
+                buttonTimer = 0;
+            }
         }
         else
         {
@@ -44,6 +84,8 @@ public class SelectionSpawner : MonoBehaviour
         if (interaction != null)
         {
             playerInTrigger = true;
+            Canvas.SetActive(true);
+            playerController.instance.enabled = false;
         }
     }
 
@@ -53,32 +95,38 @@ public class SelectionSpawner : MonoBehaviour
         if (interaction != null)
         {
             playerInTrigger = false;
+            Canvas.SetActive(false);
         }
     }
 
-    void selectEnemies()
+    void selectEverything()
     {
-        if (Input.GetKeyDown("right") && objectListPos < objectsToSpawn.Count - 1)
+        if (Input.GetKeyDown("right") && objectListPos < spawnList.Count - 1)
         {
+            rightButtonFilled.SetActive(false);
+            rightButtonHole.SetActive(true);
             objectListPos++;
-            //changeEnemies();
+            changeEverything();
         }
         if (Input.GetKeyDown("left") && objectListPos > 0)
         {
+            leftButtonFilled.SetActive(false);
+            leftButtonHole.SetActive(true);
             objectListPos--;
-            //changeEnemies();
+            changeEverything();
         }
     }
 
-    void changeEnemies()
+    void changeEverything()
     {
-        //objects = objectsToSpawn[objectListPos];
+        image.sprite = spawnList[objectListPos].sprite;
+        spawnObject = spawnList[objectListPos].pickup;
     }
 
     void spawn()
     {
         int arrayPos = Random.Range(0, spawnPos.Length);
-        Instantiate(objectsToSpawn[objectListPos], spawnPos[arrayPos].position, spawnPos[arrayPos].rotation);
+        Instantiate(spawnObject, spawnPos[arrayPos].position, spawnPos[arrayPos].rotation);
         spawnCount++;
         spawnTimer = 0;
     }
---
+++ TheGame/Assets/Scripts/spawnStats.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class spawnStats : ScriptableObject
+{
+    public GameObject pickup;
+    public Sprite sprite;
+}
