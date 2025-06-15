d1091106ea6ffecec24c4917fba11cef6687d0c6
TristinS5 <tlsneary@student.fullsail.edu>
2025-06-15 02:45:00 -0400

    Fixed and re added a DialogueStats and gameEventManager

--- TheGame/Assets/Scripts/gameEventManager.cs
+++ TheGame/Assets/Scripts/gameEventManager.cs
@@ -1,35 +1,35 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-//public class gameEventManager : MonoBehaviour
-//{
-//    public static gameEventManager instance;
+public class gameEventManager : MonoBehaviour
+{
+    public static gameEventManager instance;
 
-//    //[SerializeField] List<DialogueStats> dialogueStatsList = new List<DialogueStats>();
-//    [SerializeField] List<GameObject> FairyTriggerList = new List<GameObject>();
+    [SerializeField] List<DialogueStats> dialogueStatsList = new List<DialogueStats>();
+    [SerializeField] List<GameObject> FairyTriggerList = new List<GameObject>();
 
-//    int ListPos = 0;
+    int ListPos = 0;
 
-//    // Start is called once before the first execution of Update after the MonoBehaviour is created
-//    void Awake()
-//    {
-//        instance = this;
-//        if (dialogueStatsList != null && FairyTriggerList != null)
-//        {
-//            for (; ListPos < dialogueStatsList.Count;)
-//            {
-//                for (int index = 0; index < dialogueStatsList[ListPos].dialogue.Count; index++)
-//                {
-//                    FairyTriggerList[ListPos].GetComponent<SelectionSpawner>().dialogue.Add(dialogueStatsList[ListPos].dialogue[index]);
-//                }
-//                ListPos++;
-//            }
-//            ListPos = 0;
-//        }
-//    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+        if (dialogueStatsList != null && FairyTriggerList != null)
+        {
+            for (; ListPos < dialogueStatsList.Count;)
+            {
+                for (int index = 0; index < dialogueStatsList[ListPos].dialogue.Count; index++)
+                {
+                    FairyTriggerList[ListPos].GetComponent<SelectionSpawner>().dialogue.Add(dialogueStatsList[ListPos].dialogue[index]);
+                }
+                ListPos++;
+            }
+            ListPos = 0;
+        }
+    }
 
-//    public void EventOff(GameObject Spawner)
-//    {
-//        Spawner.SetActive(false);
-//    }
-//}
+    public void EventOff(GameObject Spawner)
+    {
+        Spawner.SetActive(false);
+    }
+}
