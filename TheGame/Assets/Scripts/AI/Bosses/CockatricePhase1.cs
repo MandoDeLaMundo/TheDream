5cd1428b5194bed81229ce4749e1e70050c6750a
Montiel_Armando <armtiel12@gmail.com>
2025-06-21 12:42:20 -0400

    DELETED scripts and ADDED separate boss phase scripts

--- TheGame/Assets/Scripts/AI/Bosses/CockatricePhase1.cs
+++ TheGame/Assets/Scripts/AI/Bosses/CockatricePhase1.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BossPhase2State : MonoBehaviour
+public class CockatricePhase1 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
